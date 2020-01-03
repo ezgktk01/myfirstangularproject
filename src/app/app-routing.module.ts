ad8ba83f7b3e740b991fa5771ded902a61ceb30f
ezgi kütük <ezgikutuk3@gmail.com>
2020-01-03 20:27:35 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ContentComponent} from './components/content/content.component';
+import {AboutComponent} from './components/about/about.component';
+import {Content} from '@angular/compiler/src/render3/r3_ast';
+import {ContactMapComponent} from './components/contact-map/contact-map.component';
+import {ContactComponent} from './components/contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ContentComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContentComponent } from './components/content/content.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ContactMapComponent } from './components/contact-map/contact-map.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    AboutComponent,
+    ContactComponent,
+    ContactMapComponent,
+    JumbotronComponent,
   ],
   imports: [
     BrowserModule,
